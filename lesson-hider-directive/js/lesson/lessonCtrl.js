ef25ffb9f37dd2755190a55f16e3a24365f968ab
Phillip Puckett <phillippuckett88@gmail.com>
2016-01-05 11:48:11 -0700

    Lesson Hider 3/7+

--- lesson-hider-directive/js/lesson/lessonCtrl.js
+++ lesson-hider-directive/js/lesson/lessonCtrl.js
@@ -1,4 +1,16 @@
 angular.module("lessonHider")
     .controller("lessonCtrl", function ($scope) {
         $scope.test = "Lesson Hider";
+        $scope.lessons =
+        [
+            'Services',
+            'Routing',
+            'Directives',
+            'Review',
+            'Firebase',
+            'No server project',
+            'Node',
+            'Express',
+            'Mongo'
+        ];
     });
--- lesson-hider-directive/js/lesson/lessonDirective.js
+++ lesson-hider-directive/js/lesson/lessonDirective.js
@@ -2,6 +2,9 @@ angular.module("lessonHider")
     .directive("lessonDirective", function () {
         return {
             templateUrl: "js/lesson/lessonTmpl.html",
-            restrict: 'E'
+            restrict: 'E',
+            link: function (scope, element, attributes) {
+                // console.log(scope, element, attributes);
+            }
         }
     });
